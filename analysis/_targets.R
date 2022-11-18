1799a3f88bdf58da8e8a235d4bd3930d0b14baad
Thomas Klebel <t.klebel@hotmail.com>
2022-11-18 06:11:21 +0100

    start with new data and quarto doc

--- analysis/_targets.R
+++ analysis/_targets.R
@@ -10,13 +10,18 @@ list(
   # baseline file -----------
   tar_target(
     baseline_file,
-    "../outputs/data_sharing_policies baseline-table-2.csv",
+    "../outputs/data_sharing_policies baseline-table-3.csv",
     format = "file"
   ),
   tar_target(
     baseline,
-    read_experiments(baseline_file), 
+    read_experiments(baseline_file),
     format = "feather"
   ),
-  tar_quarto(baseline_report, "01-analyse-baseline.qmd")
+  tar_target(
+    baseline_selection,
+    select_baseline(baseline),
+    format = "feather"
+  ),
+  tarchetypes::tar_quarto(baseline_report, "01-analyse-baseline.qmd")
 )
