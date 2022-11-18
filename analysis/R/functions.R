4ea890d60c964c0888f5dfffc5bd4e9191c7ee55
Thomas Klebel <t.klebel@hotmail.com>
2022-11-18 06:12:33 +0100

    new function for selecting columns in baseline report

--- analysis/R/functions.R
+++ analysis/R/functions.R
@@ -3,3 +3,16 @@ read_experiments <- function(path) {
     tibble(.name_repair = "universal") %>% 
     mutate(across(contains("gini"), as.numeric))
 }
+
+select_baseline <- function(df) {
+  df %>% 
+    select(run_number = .run.number., init_dist = resources.dist, network, 
+           funded_share = funded.share, step = .step., 
+           resources_gini = gini..resources..of.teams,
+           mean_effort = mean..effort..of.teams, perc_sharing = ..sharing,
+           mean_funds_q1 = mean.funding.within.teams.with..initial.resources.quantile....q1..,
+           mean_funds_q2 = mean.funding.within.teams.with..initial.resources.quantile....q2..,
+           mean_funds_q3 = mean.funding.within.teams.with..initial.resources.quantile....q3..,
+           mean_funds_q4 = mean.funding.within.teams.with..initial.resources.quantile....q4..,
+           )
+}
