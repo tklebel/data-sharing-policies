aed8d3e4f1de5a52d7b01da01f3acfaca3d000e0
Thomas Klebel <t.klebel@hotmail.com>
2022-12-14 06:25:43 +0100

    update file paths with new simulation runs

--- analysis/_targets.R
+++ analysis/_targets.R
@@ -10,14 +10,14 @@ list(
   # baseline file -----------
   tar_target(
     baseline_file,
-    "../outputs/data_sharing_policies baseline-table-2.csv",
+    "../outputs/data_sharing_policies baseline-table.csv",
     format = "file"
   ),
   tarchetypes::tar_quarto(baseline_report, "01-analyse-baseline.qmd"),
   # funding intervention -----------
   tar_target(
     intervention_file,
-    "../outputs/data_sharing_policies vary_incentives-table-3.csv",
+    "../outputs/data_sharing_policies vary_incentives-table.csv",
     format = "file"
   ),
   tarchetypes::tar_quarto(funding_intervention, "02-analyse-funding-intervention.qmd")
