ebbf2c0f18e5b5918e77e5c6d436711b574bcdbb
Thomas Klebel <t.klebel@hotmail.com>
2023-05-09 11:20:15 +0200

    commit old changes
    these were adaptions to the new model formulation. sidestepping the targets pipeline somewhat.

--- analysis/_targets.R
+++ analysis/_targets.R
@@ -9,17 +9,17 @@ tar_option_set(packages = c("scales", "tidyverse", "hrbrthemes"))
 
 list(
   # baseline file -----------
-  tar_target(
-    baseline_file,
-    "../outputs/data_sharing_policies baseline-table.csv",
-    format = "file"
-  ),
+  # tar_target(
+  #   baseline_file,
+  #   "../outputs/data_sharing_policies baseline-table.csv",
+  #   format = "file"
+  # ),
   tarchetypes::tar_quarto(baseline_report, "01-analyse-baseline.qmd"),
   # funding intervention -----------
-  tar_target(
-    intervention_file,
-    "../outputs/data_sharing_policies vary_incentives-table.csv",
-    format = "file"
-  ),
+  # tar_target(
+  #   intervention_file,
+  #   "../outputs/data_sharing_policies vary_incentives-table.csv",
+  #   format = "file"
+  # ),
   tarchetypes::tar_quarto(funding_intervention, "02-analyse-funding-intervention.qmd")
 )
