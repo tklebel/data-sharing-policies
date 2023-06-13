1860329ef392c595befdc03584faf6fbf21b0cf6
Thomas Klebel <t.klebel@hotmail.com>
2023-06-13 11:42:54 +0200

    fiddle further with graph generation

--- network_generation/00-generate-networks.R
+++ network_generation/00-generate-networks.R
@@ -2,6 +2,59 @@ library(igraph)
 library(ggraph)
 library(tidygraph)
 library(tidyverse)
+library(ggtext)
+
+extrafont::loadfonts(device = "win")
+
+
+plot_graph <- function(graph, layout = "stress") {
+  graph %>% 
+    activate(nodes) %>% 
+    mutate(group = group_louvain(),
+           degree = centrality_degree(),
+           page_rank = centrality_pagerank()) %>% 
+    ggraph(layout) +
+    geom_edge_link() +
+    geom_node_point(aes(colour = as.factor(group),
+                        size = page_rank)) +
+    theme_graph() +
+    theme(legend.position = "none")
+}
+
+summarise_graph <- function(graph) {
+  clustering <- graph %>% transitivity()
+  mean_distance <- graph %>% mean_distance()
+  
+  graph %>%   
+    activate(nodes) %>% 
+    mutate(degree = centrality_degree()) %>% 
+    as_tibble() %>% 
+    summarise(avg_degree = mean(degree),
+              clustering = clustering, mean_dist = mean_distance)
+}
+
+
+
+plot_degree <- function(graph) {
+  degree_dist <- graph %>% 
+    activate(nodes) %>% 
+    mutate(degree = centrality_degree()) %>% 
+    as_tibble() %>% 
+    count(degree)
+  
+  
+  degree_dist %>% 
+    ggplot(aes(degree, n)) +
+    geom_point() +
+    geom_line() +
+    theme_bw() +
+    labs(x = "number of collaborators *k* (degree)",
+         y = "number of teams with *k* collaborators") +
+    theme(axis.title.x = ggtext::element_markdown(),
+          axis.title.y = ggtext::element_markdown())
+}
+
+
 
 
 sw <- sample_smallworld(dim = 1, size = 100, nei = 2, p = .01)
@@ -68,7 +121,7 @@ physics %>% transitivity()
 new_p %>% 
   activate(nodes) %>% 
   mutate(group = group_louvain()) %>% 
-  ggraph("kk") +
+  ggraph() +
   geom_edge_link() +
   geom_node_point(aes(colour = as.factor(group)), size = 3) +
   theme_graph()
@@ -80,39 +133,105 @@ new_p %>%
 # try with fewer initial edges, adding more edges via BA model -> this does not
 # work well (with setting seed_size = 60. leads to lower transitivity, but this
 # might be fine for some disciplines?)
-seed_size <- 60
-base_rewire <- .01
+seed_size <- 40
+base_rewire <- .001
 physics_setup <- tibble(
-  n_dim = rep(1, 4),
-  dim_size = c(seed_size * .1, seed_size * .2, seed_size * .35, seed_size * .4),
-  order = c(3, 4, 4, 5), # neighborhood size
-  p_rewire = c(base_rewire, base_rewire, base_rewire * .2, base_rewire * .15)
+  n_dim = rep(1, 3),
+  dim_size = c(seed_size * .2, seed_size * .35, seed_size * .5),
+  order = c(2, 3, 4), # neighborhood size
+  p_rewire = c(base_rewire, base_rewire * .2, base_rewire * .15)
 )
 
 physics_graphs <- pmap(physics_setup, play_smallworld)
 physics <- bind_graphs(physics_graphs)
 
-new_p <- sample_pa(100, start.graph = physics, out.pref = TRUE, m = 10, power = 3,
+new_p <- sample_pa(100, start.graph = physics, out.pref = TRUE, m = 5, power = 1,
                    directed = FALSE) %>% 
   as_tbl_graph()
 
-new_p %>% 
-  activate(nodes) %>% 
-  mutate(group = group_infomap(),
-         degree = centrality_degree(),
-         page_rank = centrality_pagerank()) %>% 
-  ggraph("kk") +
-  geom_edge_link() +
-  geom_node_point(aes(colour = as.factor(group),
-                      size = page_rank)) +
-  theme_graph()
+plot_graph(new_p)
 # the current issue is that most added edges appear in the largest and densest
 # component
 # a solution could be to have more but smaller groups
-new_p %>% transitivity()
-physics %>% transitivity()
+summarise_graph(new_p)
+plot_degree(new_p)
+
+# two new approaches to try:
+# - add edges to a few random nodes, and only then add scale free edges
+# - generate second network with more clusters
 
 
+# fragmented network ------
+seed_size <- 90
+base_rewire <- .001
+fragmented_setup <- tibble(
+  n_dim = rep(1, 7),
+  dim_size = round(
+    c(seed_size * .1, seed_size * .1, seed_size * .2, seed_size * .3,
+               seed_size * .05, seed_size * .1, seed_size * .15)
+    ),
+  order = c(2, 3, 3, 4, 4, 5, 5), # neighborhood size
+  p_rewire = c(base_rewire, base_rewire, base_rewire, base_rewire,
+               base_rewire * .2, base_rewire * .15, base_rewire * .1)
+)
+
+fragmented_graphs <- pmap(fragmented_setup, play_smallworld)
+fragmented <- bind_graphs(fragmented_graphs)
+
+new_frag <- sample_pa(100, start.graph = fragmented, out.pref = TRUE, m = 5,
+                      power = 1, directed = FALSE) %>% 
+  as_tbl_graph()
+
+plot_graph(new_frag)
+summarise_graph(new_frag)
+plot_degree(new_frag)
+# the neighborhood size seems to be too large, there is too much clustering
+# (but this might fit well to physics?)
+# 
+
+# besides:
+# the algorithm does not work as anticipated. edges are only added for nodes
+# that are added. but I wanted to add edges to existing nodes too. will have to
+# do this myself
+# pseudo-code
+# pick random node
+# pick another node according to degree (same algorithm as in barbasi albert)
+# create edge from random to second node. this should start introducing some
+# further clustering
+# 
+# if this is not leading to enough clustering, either the probability would need
+# to be skewed (similar to the power argument above), or we would need to pick
+# a couple of nodes ahead of time, and add many more edges to them
+
+# islands algorithm
+islandic <- play_islands(5, 25, p_within = .4, m_between = 4)
+summarise_graph(islandic)
+plot_graph(islandic)
+
+islandic %>% 
+  activate(nodes) %>% 
+  mutate(degree = centrality_degree()) %>% 
+  as_tibble() %>% 
+  summarise(avg_degree = mean(degree))
+
+
+# the islandic approach is too barebone. The clustered thing that I currently
+# get in both other approaches is fine. Just need an additional one that has
+# much lower clustering, and lower degree, i.e., a more disconnected network
+# 
+# what might also be missing is the power-law aspect would need to look at the
+# degree distribution and make such a plot? or can the value be calculated?
+
+
+# plot degree distribution
+new_frag %>% degree()
+plot_degree(new_frag)
+
+
+
+
+
+  
 # bipartite ----
 # bp <- sample_bipartite(50, 50, p = .1)
 bp <- sample_bipartite(50, 50, type = "gnm", m = 400)
