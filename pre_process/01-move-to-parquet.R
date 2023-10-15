25dd624e81fdb92a6ab9d3f521401aba614c4d45
Thomas Klebel <t.klebel@hotmail.com>
2023-10-15 10:26:09 +0200

    update scripts for processing simulation output

--- pre_process/01-move-to-parquet.R
+++ pre_process/01-move-to-parquet.R
@@ -1,3 +1,4 @@
+source("renv/activate.R")
 library(sparklyr)
 library(arrow)
 library(dplyr)
@@ -11,7 +12,7 @@ Sys.setenv(JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64")
 
 config <- spark_config()
 config$spark.executor.cores <- 5
-config$spark.executor.instances <- 30
+config$spark.executor.instances <- 38
 config$spark.executor.memory <- "15G"
 config$spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version <- 2
 
@@ -19,7 +20,7 @@ options(sparklyr.log.console = FALSE)
 
 sc <- spark_connect(master = "yarn", config = config,
                     app_name = "convert_to_parquet")
-# 
+
 # spark_read_csv(sc, name = "source_csv",
 #                path = "/tklebel/data_sharing_abm/vary_incentives_individuals_no_network.csv.bz2",
 #                memory = FALSE)
@@ -27,17 +28,17 @@ sc <- spark_connect(master = "yarn", config = config,
 # temp_df <- tbl(sc, "source_csv")
 # 
 # # drop data that is redundant
-# selected_cols <- temp_df %>% 
-#   select(run_number, maxinitialutility, step, individualdata, fundedshare, 
-#          sharingincentive)
+# selected_cols <- temp_df %>%
+#   select(run_number, step, maxinitialutility, fundedshare, sharingincentive,
+#          individualdata)
 # 
 # spark_write_parquet(
-#   selected_cols, 
+#   selected_cols,
 #   path = "/tklebel/data_sharing_abm/vary_incentives_individuals_no_network.parquet",
 #   mode = "overwrite"
 # )
-# 
-# 
+
+
 # # random network
 # spark_read_csv(sc, name = "source_csv",
 #                path = "/tklebel/data_sharing_abm/vary_incentives_individuals_random_network.csv.bz2",
@@ -46,52 +47,52 @@ sc <- spark_connect(master = "yarn", config = config,
 # temp_df <- tbl(sc, "source_csv")
 # 
 # # drop data that is redundant
-# selected_cols <- temp_df %>% 
-#   select(run_number, maxinitialutility, step, individualdata, fundedshare, 
-#          sharingincentive)
+# selected_cols <- temp_df %>%
+  # select(run_number, step, maxinitialutility, fundedshare, sharingincentive, 
+  #        individualdata)
 # 
 # spark_write_parquet(
-#   selected_cols, 
+#   selected_cols,
 #   path = "/tklebel/data_sharing_abm/vary_incentives_individuals_random_network.parquet",
 #   mode = "overwrite"
 # )
-
+# 
 # fragmented network
-# spark_read_csv(sc, name = "source_csv",
-#               path = "/tklebel/data_sharing_abm/vary_incentives_individuals_fragmented.csv.bz2",
-#               memory = FALSE)
-#
-#temp_df <- tbl(sc, "source_csv")
-
-# drop data that is redundant
-#selected_cols <- temp_df %>% 
-#  select(run_number, maxinitialutility, step, individualdata, fundedshare, 
-#         sharingincentive)
-#
-#spark_write_parquet(
-#  selected_cols, 
-#  path = "/tklebel/data_sharing_abm/vary_incentives_individuals_fragmented.parquet",
-#  mode = "overwrite"
-#)
-
-
-# clustered network
 spark_read_csv(sc, name = "source_csv",
-               path = "/tklebel/data_sharing_abm/vary_incentives_individuals_clustered.csv.bz2",
-               memory = FALSE)
+              path = "/tklebel/data_sharing_abm/vary_incentives_individuals_fragmented.csv.bz2",
+              memory = FALSE)
 
 temp_df <- tbl(sc, "source_csv")
 
-# drop data that is redundant
-selected_cols <- temp_df %>% 
-  select(run_number, maxinitialutility, step, individualdata, fundedshare, 
-         sharingincentive)
+#  drop data that is redundant
+selected_cols <- temp_df %>%
+  select(run_number, step, maxinitialutility, fundedshare, sharingincentive,
+         individualdata)
 
 spark_write_parquet(
-  selected_cols, 
-  path = "/tklebel/data_sharing_abm/vary_incentives_individuals_clustered.parquet",
-  mode = "overwrite"
+ selected_cols,
+ path = "/tklebel/data_sharing_abm/vary_incentives_individuals_fragmented.parquet",
+ mode = "overwrite"
 )
+
+
+# # clustered network
+# spark_read_csv(sc, name = "source_csv",
+#                path = "/tklebel/data_sharing_abm/vary_incentives_individuals_clustered.csv.bz2",
+#                memory = FALSE)
+# 
+# temp_df <- tbl(sc, "source_csv")
+# 
+# # drop data that is redundant
+# selected_cols <- temp_df %>%
+#   select(run_number, step, maxinitialutility, fundedshare, sharingincentive,
+#          individualdata)
+# 
+# spark_write_parquet(
+#   selected_cols,
+#   path = "/tklebel/data_sharing_abm/vary_incentives_individuals_clustered.parquet",
+#   mode = "overwrite"
+# )
 
 
 
