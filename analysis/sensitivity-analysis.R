174b0edac5399f030642809edeee85ae2051fabf
Thomas Klebel <t.klebel@hotmail.com>
2025-04-02 14:48:37 +0200

    update analysis script

--- analysis/sensitivity-analysis.R
+++ analysis/sensitivity-analysis.R
@@ -2,6 +2,7 @@ library(sparklyr)
 library(dplyr)
 library(here)
 library(ggplot2)
+library(arrow)
 
 # Port forwarding for better UI access:
 #   ssh -L 4040:localhost:4040 username@your-server
@@ -9,98 +10,53 @@ library(ggplot2)
 # Simple configuration - minimal settings to avoid conflicts
 options(sparklyr.log.console = TRUE)
 
-
-# conf <- spark_config()
-# conf$spark.driver.memory <- "45g"
-# conf$`sparklyr.shell.driver-memory` <- '30G'
-# conf$`sparklyr.shell.executor-memory` <- '30G'
-# 
-# # Connect using Spark 3.5.0 explicitly
-# sc <- spark_connect(master = "local[8]", config = conf, version = "3.5.0")
-# # Check JVM settings 
-# system("jps -lvm")
-
-
-
-
 conf <- spark_config()
 
+# Memory allocation - scaled proportionally (75% of available RAM)
+conf$spark.driver.memory <- "96g"                    # From 48g to 96g
+conf$`sparklyr.shell.driver-memory` <- "96g"         # From 48g to 96g
 
-# Memory allocation - more conservative to allow for spillage
-conf$spark.driver.memory <- "48g"
-conf$`sparklyr.shell.driver-memory` <- "48g"
+# Increase result size limit proportionally
+conf$spark.driver.maxResultSize <- "16g"             # From 8g to 16g
 
-# Critical for large files - increase sort spillage threshold
-conf$spark.driver.maxResultSize <- "8g"  # Prevent large collect() operations from failing
+# File processing adjustments
+conf$spark.sql.files.maxPartitionBytes <- "256m"     # Keeping same (good balance)
+conf$spark.sql.shuffle.partitions <- 224             # From 112 to 224 (8 × cores)
 
-# File processing for large files
-conf$spark.sql.files.maxPartitionBytes <- "256m"  # Balance between parallelism and overhead
-conf$spark.sql.shuffle.partitions <- 112  # 8 × cores for complex operations on big data
-conf$spark.sql.adaptive.enabled <- "true"  # Crucial for large data processing
+# Keeping these adaptive settings (they work well)
+conf$spark.sql.adaptive.enabled <- "true"
 conf$spark.sql.adaptive.coalescePartitions.enabled <- "true"
 conf$spark.sql.adaptive.skewJoin.enabled <- "true"
 
-# Disk spillage settings (crucial for large files)
+# Keeping disk spillage settings (crucial for large files)
 conf$spark.sql.shuffle.spill <- "true"
 conf$spark.shuffle.spill.compress <- "true"
 conf$spark.shuffle.compress <- "true"
-conf$spark.shuffle.file.buffer <- "1m"  # Larger buffer for better I/O performance
+conf$spark.shuffle.file.buffer <- "1m"
 
-# Smaller broadcast threshold helps with join operations on large data
-conf$spark.sql.autoBroadcastJoinThreshold <- "64m"
+# Slightly increased broadcast threshold
+conf$spark.sql.autoBroadcastJoinThreshold <- "128m"  # From 64m to 128m
 
-# Serialization (faster but uses more memory)
+# Keeping serialization settings
 conf$spark.serializer <- "org.apache.spark.serializer.KryoSerializer"
 
-# Enable speculative execution for long-running tasks
+# Keeping speculative execution settings
 conf$spark.speculation <- "true"
-
 conf$spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version <- "2"
 
-# CPU settings - use most of your cores but leave some headroom
-sc <- spark_connect(master = "local[14]", config = conf, version = "3.5.0")
+# Use arrow for faster file transfer
+conf$spark.sql.execution.arrow.enabled <- "true"
+conf$spark.sql.execution.arrow.maxRecordsPerBatch <- "100000"  # Adjust based on your memory
 
-spark_web(sc)
+# CPU settings - using same proportion of cores but more total
+sc <- spark_connect(master = "local[28]", config = conf, version = "3.5.0")
 
-
-# sensitive <- spark_read_csv(sc, "sensitivity",
-#                             path = here("outputs/sharing-costs-sensitivity.csv.bz2"),
-#                             memory = FALSE)
-
-
-# initialnorm = 0
-# resourcedist = uniform
-# applicationpenalty = 0.05
-# proposalsigma ) 0.15
-# nteams = 100
-# thirdpartyfundingratio = 2
-# 
-
-# core_set <- sensitive %>% 
-#   select(run_number, step, sharingcostscap, sharingincentive, network, fundedshare, datasharing, sharing) 
-#   
-
-# summarised_table <- core_set %>%
-#   # head(200) %>% 
-#   group_by(sharingcostscap, sharingincentive, network, fundedshare, step) %>%
-#   summarise(n = n(),
-#             mean_sharing = mean(sharing),
-#             sd_sharing = sd(sharing)) %>%
-#   collect()
-
-
-# spark_write_parquet(core_set, "outputs/sharing-costs-sensitivity.parquet",
-#                     mode = "overwrite",
-#                     partition_by = c("network", "sharingcostscap")
-# )
-
-
-
-core_set <- spark_read_parquet(sc, name = "core_set_costs_cap", 
+# read the data
+overview_set <- spark_read_parquet(sc, name = "overview_set_costs_cap", 
                                path = "outputs/sharing-costs-sensitivity.parquet/",
                                memory = FALSE)
 
-summarised_table <- core_set %>%
+summarised_table <- overview_set %>%
     # head(200) %>%
     group_by(sharingcostscap, sharingincentive, network, fundedshare, step) %>%
     summarise(n = n(),
@@ -120,5 +76,29 @@ ggsave("analysis/sensitivity_plot.png", width = 15, height = 15)
 # only for low incentives (up to .3) there are meaningful differences. 
 # high costs dont really make a difference, but there is more to explore between
 # costs of 0 and .1
+
+# higher resolution
+high_res_set <- spark_read_parquet(sc, name = "high_res_set", 
+                                   path = "outputs/sharing-costs-sensitivity_high_res.parquet/",
+                                   memory = FALSE)
+
+summarised_table_high_res <- high_res_set %>%
+  # head(200) %>%
+  group_by(sharingcostscap, sharingincentive, network, fundedshare, step) %>%
+  summarise(n = n(),
+            mean_sharing = mean(sharing),
+            sd_sharing = sd(sharing)) %>%
+  spark_write_parquet("interim_outputs/sharing_cap_high_res.parquet")
+  collect()
+
+arrow::write_csv_arrow(summarised_table_high_res, "interim_outputs/sharing_cap_high_res.csv")
+
+summarised_table_high_res %>% 
+  filter(network == "none") %>% 
+  ggplot(aes(step, mean_sharing, colour = as.factor(sharingcostscap), group = sharingcostscap)) +
+  geom_line() +
+  facet_grid(rows = vars(fundedshare),
+             cols = vars(sharingincentive))
+ggsave("analysis/sensitivit_high_res.png", width = 15, height = 15)
 
 spark_disconnect(sc)
